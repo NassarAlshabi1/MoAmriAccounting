bbc110efa9dce4999d04403c3319b2b99fc1a8e8
Z User <z@container>
2026-03-10 22:12:08 +0000

    fix: تحديث لون الأيقونة للأزرق وإصلاح التحذيرات
    - تغيير لون الخلفية من #3DDC84 إلى #3d8fdc (أزرق)
    - إعادة إنشاء أيقونة التطبيق
    - إزالة الـ imports غير المستخدمة من debts_controller.dart و debts_data_source.dart

--- lib/debts/controllers/debts_controller.dart
+++ lib/debts/controllers/debts_controller.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:moamri_accounting/controllers/main_controller.dart';
 import 'package:moamri_accounting/database/debts_database.dart';
--- lib/debts/data_sources/debts_data_source.dart
+++ lib/debts/data_sources/debts_data_source.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:moamri_accounting/controllers/main_controller.dart';
 import 'package:moamri_accounting/utils/global_utils.dart';
 import 'package:syncfusion_flutter_datagrid/datagrid.dart';
